6e7d4deda99e772ab501ec57960811fb72b6a377
nine <>
2023-12-10 13:11:27 -0500

    add custom view

--- frontend/public/js/index.js
+++ frontend/public/js/index.js
@@ -1,23 +1,42 @@
 
-let orderPizza = {};
+function changePrice(customDetails, fromCustom = true) {
+  let totalPrice = customDetails.price;
 
-function changePrice() {
-  var sizeOption = $("#customSize").find(":selected");
-  var sizePrice = sizeOption.data("price");
-  orderPizza.size = sizeOption.val();
+  if (fromCustom) {
+    const sizeOption = $("#customSize").find(":selected");
+    const sizePrice = sizeOption.data("price");
+    const size = sizeOption.val();
+  
+    const toppingOption = $("#customTopping").find(":selected");
+    const toppingPrice = toppingOption.data("price");
+    const topping = toppingOption.val();
+  
+    if (size && sizePrice) {
+      customDetails.size = size;
+      totalPrice += sizePrice;
+    }
+    if (topping && toppingPrice) {
+      customDetails.topping = topping;
+      totalPrice += toppingPrice;
+    }
+  }
 
-  var toppingOption = $("#customTopping").find(":selected");
-  var toppingPrice = toppingOption.data("price");
-  orderPizza.topping = toppingOption.val();
-
-  let totalPrice = (sizePrice + toppingPrice + orderPizza.price).toFixed(2);
-  $("#customPrice").val("$" + totalPrice);
+  customDetails.totalPrice = totalPrice;
+  return customDetails;
 }
 
-function showOrderDialog() {
-  let op = orderPizza;
-  console.log("pri:" + op.price);
-  $(".dialog-pizza-image").attr("src", orderPizza.img);
+function showOrderDialog(customDetails) {
+  console.log("pri:" + customDetails.price);
+  $(".dialog-pizza-image").attr("src", customDetails.img);
+  $("#price").val(customDetails.totalPrice.toFixed(2));
+
+  $("#sizeLayout").attr("hidden", customDetails.topping == undefined)
+  $("#toppingLayout").attr("hidden", customDetails.topping == undefined)
+  $("#size").val(customDetails.size);
+  $("#topping").val(customDetails.topping);
+  
+  $("#topping").val(customDetails.topping);
+  $("#overlay-order").data("price", customDetails.totalPrice)
   $("#overlay-order").fadeIn();
 }
 
@@ -44,57 +63,76 @@ $().ready(() => {
 
     $("#list_ul").html(ul);
 
-    $('.order-button').click(function (evt) {
-      let id = $(this).data('id');
-      let img = $(this).data('img');
-      let price = $(this).data('price');
-      orderPizza = { id, img, price }
+    let customDetails = {};
 
-      showOrderDialog();
+    $('.order-button').click(function (evt) {
+      customDetails = {};
+      if (!customDetails.totalPrice) {
+        let id = $(this).data('id');
+        let img = $(this).data('img');
+        let price = $(this).data('price');
+        customDetails.id = id;
+        customDetails.img = img;
+        customDetails.price = price;
+        changePrice(customDetails, false);
+      }
+      showOrderDialog(customDetails);
     });
 
     $('.custom-button').click(function (evt) {
+      customDetails = {};
       let id = $(this).data('id');
       let img = $(this).data('img');
       let price = $(this).data('price');
-      orderPizza = { id, img, price }
+      customDetails.id = id;
+      customDetails.img = img;
+      customDetails.price = price;
+      changePrice(customDetails);
+      $("#customPrice").val("$" + customDetails.totalPrice.toFixed(2));
 
       $(".dialog-pizza-image").attr("src", img);
 
       $.get("http://localhost:4000/api/size", function (response, status) {
         console.log("response:" + response.data);
 
+        $("#customSize").html("");
         response.data.map(item => {
           console.log('i:' + item);
           $("#customSize").html($("#customSize").html() + `<option value="${item.name}" data-price="${item.price}">${item.name}</option>`)
         })
 
-        changePrice();
+        changePrice(customDetails);
+        $("#customPrice").val("$" + customDetails.totalPrice.toFixed(2));
       });
 
       $.get("http://localhost:4000/api/topping", function (response, status) {
         console.log("response:" + response.data);
 
+        $("#customTopping").html("");
         response.data.map(item => {
           console.log('i:' + item);
           $("#customTopping").html($("#customTopping").html() + `<option value="${item.name}" data-price="${item.price}">${item.name}</option>`)
         })
 
-        changePrice();
+        changePrice(customDetails);
+        $("#customPrice").val("$" + customDetails.totalPrice.toFixed(2));
       });
 
       $("#customSize").change(function () {
-        changePrice();
+        changePrice(customDetails);
+        $("#customPrice").val("$" + customDetails.totalPrice.toFixed(2));
       })
 
       $("#customTopping").change(function () {
-        // let price = $(this)["data-price"]
-        changePrice();
+        changePrice(customDetails);
+        $("#customPrice").val("$" + customDetails.totalPrice.toFixed(2));
       })
 
-      console.log("order:" + evt.target.getAttribute("data-img"));
+      $("#overlay-custom").data('id', id);
+      $("#overlay-custom").data('img', img);
+      $("#overlay-custom").data('price', price);
       $("#overlay-custom").fadeIn();
-    });
+    }); // end click custom
 
     $("#closeOrderDialog").click(function (evt) {
       $("#overlay-order").fadeOut();
@@ -106,8 +144,15 @@ $().ready(() => {
 
     $("#formCustom").submit(function (evt) {
       evt.preventDefault();
+      let id = $("#overlay-custom").data('id');
+      let img = $("#overlay-custom").data('img');
+      let price = $("#overlay-custom").data('price');
+      customDetails.id = id;
+      customDetails.img = img;
+      customDetails.price = price;
+      changePrice(customDetails);
       $("#overlay-custom").fadeOut();
-      showOrderDialog();
+      showOrderDialog(customDetails);
     });
   });
 })
