440ebe785fd9d22a72d927978edcb0e714bb49a3
nine <>
2023-12-11 11:34:15 -0500

    add import pdf button

--- frontend/public/js/orders.js
+++ frontend/public/js/orders.js
@@ -6,18 +6,40 @@ $().ready(() => {
         console.log("response:" + JSON.stringify(response));
 
         let ol = "";
-        response.data.forEach(item => {
-            let topping = item.topping ? item.topping : "";
-            ol += `
-                <tr>
-                    <td><img src="${item.picUrl}" alt="${item.description}" /></td>
-                    <td>${item.name}</td>
-                    <td>${item.size}</td>
-                    <td>${topping}</td>
-                    <td>${item.price}</td>
-                    <td>${item.address? item.address: ""}</td>
-                </tr>
-        `
+        response.data.forEach(ps => {
+
+            ps.items.forEach(item => {
+
+                let pizza = item.pizza;
+                let size = item.size;
+                let toppings = item.toppings;
+
+                let price = pizza.price;
+                if (size) {
+                    price += size.price;
+                    size = size.name;
+                } else {
+                    size = "";
+                }
+
+                let topping = "";
+                toppings.forEach(topping => {
+                    price += topping.price;
+                    topping += "," + topping.name;
+                });
+                topping = topping.slice(1);
+
+                ol += `
+                    <tr>
+                        <td><img src="${pizza.picUrl}" class="img-thumbnail" alt="${pizza.description}" /></td>
+                        <td>${pizza.name}</td>
+                        <td>${size}</td>
+                        <td>${topping}</td>
+                        <td>${price}</td>
+                        <td>${item.address ? item.address : ""}</td>
+                    </tr>
+                `
+            });
         });
 
         $("#tableBody").html(ol);
